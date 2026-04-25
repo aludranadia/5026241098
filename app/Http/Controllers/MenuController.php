fc76dcaa16229cd96a19f6bc3ea0333d626bf055
aludranadia <nadiaaludra06@gmail.com>
2026-04-25 12:18:26 +0700

    tugas pertemuan 7

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    //
+    public function index(){ //Method index untuk menampilkan halaman menu utama yang berisi kumpulan link tugas pertemuan 1 - 6
+        return view('menu');
+    }
+
+    public function idx(){ //Method idx untuk menampilkan halaman index, begitu pula untuk halaman lainnya
+        return view('index'); //Penamaan 'index' harus konsisten dengan nama file blade, begitu pula untuk halaman lainnya
+    }
+
+    public function example(){
+        return view('contoh');
+    }
+
+    public function int(){
+        return view('intro');
+    }
+
+    public function linktr(){
+        return view('linktree');
+    }
+
+    public function berita1(){
+        return view('news');
+    }
+
+    public function berita2(){
+        return view('news1');
+    }
+
+    public function pert4(){
+        return view('pertemuan4');
+    }
+
+    public function pert5(){
+        return view('pertemuan5');
+    }
+
+    public function respons(){
+        return view('responsive');
+    }
+
+    public function temp(){
+        return view('template');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\MenuController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,9 +15,19 @@ Route::get('halo', function () {
 Route::get('blog', function () {
 	return view('blog');
 });
-Route::get('pert5', function () {
-	return view('pertemuan5');
-});
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+//Route untuk Menu Pertemuan 1 - 6
+Route::get('menu', [MenuController::class, 'index']); //Route untuk menampilkan halaman menu utama yang berisi kumpulan link tugas pertemuan 1 - 6
+Route::get('idx', [MenuController::class, 'idx']); //Direct ke MenuController. Method idx untuk menampilkan halaman index, begitu pula untuk halaman lainnya
+Route::get('example', [MenuController::class, 'example']);
+Route::get('int', [MenuController::class, 'int']);
+Route::get('linktr', [MenuController::class, 'linktr']);
+Route::get('berita1', [MenuController::class, 'berita1']);
+Route::get('berita2', [MenuController::class, 'berita2']);
+Route::get('pert4', [MenuController::class, 'pert4']);
+Route::get('pert5', [MenuController::class, 'pert5']);
+Route::get('respons', [MenuController::class, 'respons']);
+Route::get('temp', [MenuController::class, 'temp']);
